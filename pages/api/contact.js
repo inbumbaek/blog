7ccd16711bc83ad664ad28e518797fa677e7cb06
inbumbaek <inbumbaek@gmail.com>
2023-09-05 02:25:14 -0700

    let client

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -21,3 +21,35 @@ async function handler(req, res) {
       name,
       message,
     };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://maximilian:2YkcXq43KyPk0vqp@cluster0.ntrwp.mongodb.net/my-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
+  }
+}
+
+export default handler;
