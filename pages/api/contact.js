2bdd0d6c2e027b17547c252770016c4146070946
inbumbaek <inbumbaek@gmail.com>
2023-09-05 02:07:15 -0700

    contact js

---
+++ pages/api/contact.js
@@ -0,0 +1,23 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
