a8c04b4bb8ecd24213c17fc834cf280f20dfa2fe
inbumbaek <inbumbaek@gmail.com>
2023-09-09 04:34:52 -0700

    edit postdetail

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,18 @@
+import Head from "next/head";
+import { Fragment } from "react";
 import PostContent from "../../components/posts/post-detail/post-content";
 import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 function PostDetailPage(props) {
-  return <PostContent post={props.post} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name="description" content={props.post.excerpt} />
+      </Head>
+      <PostContent post={props.post} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps(context) {
@@ -25,7 +35,7 @@ export function getStaticPaths() {
   const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ""));
 
   return {
-    paths: slugs.map(slug => ({ params: { slug: slug }})),
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
     fallback: false,
   };
 }
