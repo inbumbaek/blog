d32b19960107b55999c0d9383f906026b8c51d25
inbumbaek <inbumbaek@gmail.com>
2023-09-12 03:23:08 -0700

    finish postcontent

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -55,3 +55,13 @@ function PostContent(props) {
       );
     },
   };
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
+}
+
+export default PostContent;
